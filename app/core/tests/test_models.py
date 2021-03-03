ea48faa0a48242904b6481a3661a4c7328ec3304
dipen <dipen.2052@gmail.com>
2021-03-03 13:58:18 +0545

    added custom user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with email is successful"""
+        email = 'dipen.2052@gmail.com'
+        password = 'dipen.1234'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalizer(self):
+        """Test the eamil for new user is normalized"""
+        email = "dipen.2052@GMAIL.COM"
+        user = get_user_model().objects.create_user(email, 'test1234')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """creating User with no email, rises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test1234')
+
+    def test_create_new_super_user(self):
+        """Test creating a new super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@diepn.com',
+            'test1234'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
